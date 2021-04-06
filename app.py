b6bddd964975b342268c82fc9247d08ec6ba33a8
Mustafa Bozkurt <mwolf845300@gmail.com>
2021-04-06 21:26:20 +0200

    app added

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class DataStore():
 data =DataStore()
 
 app = Flask(__name__)
-app.static_folder="static"
+
 app.config["SESSION_COOKIE_SECURE"]=False
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SECRET_KEY'] = 'analyticteam'
